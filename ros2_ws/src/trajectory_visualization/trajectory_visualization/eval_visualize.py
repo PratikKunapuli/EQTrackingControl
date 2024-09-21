d6eea1d03512b429287f9f894e8807d9a6ced65a
Jake Welde <jwelde@seas.upenn.edu>
2024-09-21 14:53:55 -0400

    small changes for vis

--- ros2_ws/src/trajectory_visualization/trajectory_visualization/eval_visualize.py
+++ ros2_ws/src/trajectory_visualization/trajectory_visualization/eval_visualize.py
@@ -4,18 +4,31 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import TransformStamped
 from tf2_ros import TransformBroadcaster
+from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 
 from math import sin, cos
 import numpy as np
 import os
 
+import sys
+
+from ament_index_python.packages import get_package_share_directory
 
 class MinimalPublisher(Node):
 
-    def __init__(self, ref_pos, ref_quat, baseline_pos, baseline_quat, eq_pos, eq_quat, real_time_factor = 1.0, frame_rate = 90.0):
+    def __init__(self, ref_pos, ref_quat, baseline_pos, baseline_quat, eq_pos, eq_quat, real_time_factor = 0.1, frame_rate = 90.0):
         super().__init__('visualizer')
         self.frame_rate = frame_rate
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+
+        # latching_qos = QoSProfile(depth=1,durability=QoSDurabilityPolicy.TRANSIENT_LOCAL)
+
+        # self.reference_description_publisher_ = self.create_publisher(String, '/reference/robot_description', 10, qos_profile=latching_qos)
+        # self.baseline_description_publisher_  = self.create_publisher(String, '/baseline/robot_description', 10, qos_profile=latching_qos)
+        # self.symmetry_description_publisher_  = self.create_publisher(String, '/symmetry/robot_description', 10, qos_profile=latching_qos)
+        # self.reference_description_publisher_ = self.create_publisher(String, '/reference/robot_description', 10)
+        # self.baseline_description_publisher_  = self.create_publisher(String, '/baseline/robot_description', 10)
+        # self.symmetry_description_publisher_  = self.create_publisher(String, '/symmetry/robot_description', 10)
+
         timer_period = 1.0/(frame_rate * real_time_factor)  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.time = 0
@@ -31,9 +44,29 @@ class MinimalPublisher(Node):
         # Initialize the transform broadcaster
         self.tf_broadcaster = TransformBroadcaster(self)
 
+        package_share_directory = get_package_share_directory('trajectory_visualization')
+
+
+        print(package_share_directory)
+
+
+        # msg = String()
+
+        # with open(package_share_directory+'/astrobee_reference.urdf', 'r') as file:
+        #     msg.data = file.read().replace('\n', '')
+        # self.reference_description_publisher_.publish(msg)
+        # with open(package_share_directory+'/astrobee_baseline.urdf', 'r') as file:
+        #     msg.data = file.read().replace('\n', '')
+        # self.baseline_description_publisher_.publish(msg)
+        # with open(package_share_directory+'/astrobee_symmetry.urdf', 'r') as file:
+        #     msg.data = file.read().replace('\n', '')
+        # self.symmetry_description_publisher_.publish(msg)
+
+
+
     def timer_callback(self):
 
-        self.get_logger().info('t=%f' % self.time)
+        self.get_logger().info('Time t=%f' % self.time)
         self.time += 1.0 / self.frame_rate
         self.index += 1
         if self.index >= 2000:
@@ -47,6 +80,8 @@ class MinimalPublisher(Node):
         self.send_pose('reference/body',self.ref_pos[self.index],self.ref_quat[self.index])
         self.send_pose('symmetry/body',self.eq_pos[self.index],self.eq_quat[self.index])
         self.send_pose('baseline/body',self.baseline_pos[self.index],self.baseline_quat[self.index])
+
+
 
     def send_pose(self, frame, p, q):
 
@@ -73,14 +108,16 @@ class MinimalPublisher(Node):
         # Send the transformation
         self.tf_broadcaster.sendTransform(t)
 
-def main(args=None):
-    ref_pos = np.load("./src/trajectory_visualization/trajectory_visualization/data/astrobee/ref_pos.npy")
-    ref_quat = np.load("./src/trajectory_visualization/trajectory_visualization/data/astrobee/ref_rotm.npy")
-    baseline_pos = np.load("./src/trajectory_visualization/trajectory_visualization/data/astrobee/baseline_pos.npy")
-    baseline_quat = np.load("./src/trajectory_visualization/trajectory_visualization/data/astrobee/baseline_rotm.npy")
-    eq_pos = np.load("./src/trajectory_visualization/trajectory_visualization/data/astrobee/equivariant_pos.npy")
-    eq_quat = np.load("./src/trajectory_visualization/trajectory_visualization/data/astrobee/equivariant_rotm.npy")
-    rclpy.init(args=args)
+def main():
+    robot = sys.argv[2]
+    print("visualizing the " + robot + "...")
+    ref_pos = np.load("./src/trajectory_visualization/trajectory_visualization/data/" + robot + "/ref_pos.npy")
+    ref_quat = np.load("./src/trajectory_visualization/trajectory_visualization/data/" + robot + "/ref_rotm.npy")
+    baseline_pos = np.load("./src/trajectory_visualization/trajectory_visualization/data/" + robot + "/baseline_pos.npy")
+    baseline_quat = np.load("./src/trajectory_visualization/trajectory_visualization/data/" + robot + "/baseline_rotm.npy")
+    eq_pos = np.load("./src/trajectory_visualization/trajectory_visualization/data/" + robot + "/equivariant_pos.npy")
+    eq_quat = np.load("./src/trajectory_visualization/trajectory_visualization/data/" + robot + "/equivariant_rotm.npy")
+    rclpy.init(args=None)
 
     minimal_publisher = MinimalPublisher(ref_pos, ref_quat, baseline_pos, baseline_quat, eq_pos, eq_quat)
 
